9e310098888460f54bbbbc67b27ecf21516206d9
gangelo <web.gma@gmail.com>
2022-12-27 17:46:46 -0500

    Fix rubocop violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# The application controller.
 class ApplicationController < ActionController::Base
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# The base application record.
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
