d30f22da53b2b236ae07a162774ef47800c5bff3
gangelo <web.gma@gmail.com>
2022-12-31 07:42:07 -0500

    WIP add specs and supporting helpers, factories, etc.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller do
+RSpec.describe HomeController do
+  describe 'GET #index' do
+    context 'when the user is not signed in' do
+      it 'redirects' do
+        get :index
+        expect(response).to have_http_status(:found)
+      end
+    end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'when the user is signed in' do
+      before do
+        sign_in_user
+      end
+
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    email { nil }
+    password { nil }
+    confirmation_token { FFaker::Random.rand(9_999_999_999) }
+    confirmation_sent_at { Time.now.utc }
+    confirmed_at { Time.now.utc }
+    reset_password_token { nil }
+    reset_password_sent_at { nil }
+  end
+
+  after(:build) do |user, evaluator|
+    user.password = FFaker::Internet.password if evaluator.password.nil?
+
+    user.email = "#{evaluator.first_name}.#{evaluator.last_name}@domain.com".downcase if evaluator.email.nil?
+    user.confirmed_at = evaluator.confirmed_at
   end
 end
