bc70b40b084caf82d1d0c021d170ad5c1791521e
gangelo <web.gma@gmail.com>
2023-01-01 12:00:27 -0500

    Fix rubocop violations

--- app/controllers/concerns/menu_presenter_concern.rb
+++ app/controllers/concerns/menu_presenter_concern.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 module MenuPresenterConcern
   extend ActiveSupport::Concern
 
   included do
-    # So that this occurs AFTER any authentication callbacks take place.
-    # This is so that this method is not needlessly called if an
-    # authentication redirect needs to take place.
-    append_before_action :initialize_menu_presenter #, unless: -> { devise_controller? }
+    # append_before_action...
+    # So that this callback occurs AFTER any authentication callbacks take place.
+    # This avoids calling #initialize_menu_presenter if an authentication redirect
+    # needs to take place.
+    append_before_action :initialize_menu_presenter
   end
 
   private
 
   def initialize_menu_presenter
-    @menu_presenter =  MenuPresenter.new(view_context)
+    @menu_presenter = MenuPresenter.new(view_context)
   end
 end
